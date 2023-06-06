16a59559143205f89e9ba487e89d8aa491a98765
Jason Ford <fordjason@gmail.com>
2023-06-06 13:28:23 -0700

    Nice little scaffold

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-import { BrowserAgent, vuePersistentComponent } from '@knowlearning/agents'
-import App from './App.vue'
+import { validate as isUUID } from 'uuid'
+import { browserAgent, vuePersistentComponent } from '@knowlearning/agents'
+import player from './player.vue'
+import editor from './editor.vue'
 
-window.Agent = BrowserAgent()
+window.Agent = browserAgent()
 
-vuePersistentComponent(App)
+async function initialize() {
+  const id = window.location.pathname.slice(1)
+  if (isUUID(id)) {
+    const sequence = await Agent.download(id).then(r => r.json())
+    vuePersistentComponent(player, sequence, id)
+  }
+  else {
+    vuePersistentComponent(editor)
+  }
+}
+
+initialize()
---
+++ player.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Player</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+</style>
